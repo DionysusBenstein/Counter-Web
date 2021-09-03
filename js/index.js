f84552fed8cd9d6eafae9f81520728d0e74bef38
DionysusBenstein <dionysusbenstein@gmail.com>
2021-09-03 22:07:36 +0300

    Add basic algorithms

---
+++ js/index.js
@@ -0,0 +1,39 @@
+const text = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.';
+
+const countWords = text => text.split(' ').length;
+
+function lengthWithoutSigns(text) {
+    const signs = "!\"#$%^&*()+=_'?.,|\\/~№:;@[]{}«»<>©";
+    let textLength = text.length;
+
+    for (let i = 0; i < text.length; i++) {
+        for (let j = 0; j < signs.length; j++) {
+            if (text[i] === signs[j])
+                textLength--;
+        }
+    }
+
+    return textLength;
+}
+
+function lengthWithoutSpaces(text) {
+    let textLength = text.length;
+
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] === ' ')
+            textLength--;
+    }
+
+    return textLength;
+}
+
+function countLines(text) {
+    let counter = 1;
+
+    for (let i = 0; i < text.length; i++) {
+        if (text[i] === '\n')
+            counter++;
+    }
+
+    return counter;
+}
